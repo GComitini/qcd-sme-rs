c40920f513f2a59c9307d30a2e9d998b72ed4355
Giorgio Comitini <giorgio.comitini@dfa.unict.it>
2024-03-20 13:01:09 +0100

    Add gluon and ghost thermal polarization integrands

--- src/low_level/oneloop/thermal/ghost.rs
+++ src/low_level/oneloop/thermal/ghost.rs
@@ -8,3 +8,29 @@
 //   be compiled into a C dynamic library. To do so, we need to double their
 //   number (one function for real arguments, another for complex arguments).
 pub(crate) mod ffi {}
+
+// For internal use only
+//
+// - Here we define the building blocks for the other functions. This module
+//   serves two purposes: to hold inlined functions and to provide a single
+//   source of truth for the actual mathematical expressions
+pub(crate) mod inlines {
+    // See if it's better not to inline these
+    use crate::low_level::oneloop::thermal::*;
+    use crate::{Num, C, R};
+
+    #[inline(always)]
+    pub fn thermal_self_energy_landau_i<T: Num>(q: R, om: T, p: R, m: R, beta: R) -> C {
+        let s = om * om + p * p;
+        let m2 = m * m;
+        let a = s + m2;
+
+        let t1 = -a * s / (2. * m2) * i_m_0_i(q, om, p, m, beta);
+        let t2 = s * s / (2. * m2) * i_0_0_i(q, om, p, beta);
+        let t3 = (s * 2. - m2) / (4. * m2) * (j_m_i(q, m, beta) - j_0_i(q, beta));
+        let t4 = a * a / 4. * d_i_m_0_i(q, om, p, m, beta);
+        let t5 = -(s - m2) / 4. * d_j_m_i(q, m, beta);
+
+        t5 + (t3 + t1) + t2 + t4
+    }
+}
--- src/low_level/oneloop/thermal/gluon.rs
+++ src/low_level/oneloop/thermal/gluon.rs
@@ -8,3 +8,81 @@
 //   be compiled into a C dynamic library. To do so, we need to double their
 //   number (one function for real arguments, another for complex arguments).
 pub(crate) mod ffi {}
+
+// For internal use only
+//
+// - Here we define the building blocks for the other functions. This module
+//   serves two purposes: to hold inlined functions and to provide a single
+//   source of truth for the actual mathematical expressions
+pub(crate) mod inlines {
+    // See if it's better not to inline these
+    use crate::low_level::oneloop::thermal::*;
+    use crate::{Num, C, R};
+
+    #[inline(always)]
+    pub fn thermal_polarization_l_landau_i<T: Num>(q: R, om: T, p: R, m: R, beta: R) -> C {
+        let s = om * om + p * p;
+        let s2 = s * s;
+        let m2 = m * m;
+        let m4 = m2 * m2;
+        let a = s + m2 * 2.;
+        let b = s + m2;
+
+        let j_m_l_p_i = s.inv() * (om * om * j_m_t_i(q, m, beta) + p * p * j_m_l_i(q, m, beta));
+        let j_0_l_p_i = s.inv() * (om * om * j_0_t_i(q, beta) + p * p * j_0_l_i(q, beta));
+        let d_j_m_l_p_i =
+            s.inv() * (om * om * d_j_m_t_i(q, m, beta) + p * p * d_j_m_l_i(q, m, beta));
+        let d2_j_m_l_p_i =
+            s.inv() * (om * om * d2_j_m_t_i(q, m, beta) + p * p * d2_j_m_l_i(q, m, beta));
+
+        let t1 = (s2 * 3. / (m * 2.) - 1.) * i_0_0_l_i(q, om, p, beta);
+        let t2 = ((s2 * 3. + s * m2 * 8. + 4. * m4) / (2. * m2) + 4.)
+            * i_m_m_l_i_same_mass(q, om, p, m, beta);
+        let t3 = -(s2 * 3. + s * m2 * 4. + m4) / m4 * i_m_0_l_i(q, om, p, m, beta);
+        let t4 = a * s * 2. / m2 * i_m_m_i_same_mass(q, om, p, m, beta);
+        let t5 = -b * s * 2. / m2 * i_m_0_i(q, om, p, m, beta);
+        let t6 = -(s * 2. + m2 * 3.) / m2 * j_m_i(q, m, beta);
+        let t7 = (s * 2. + m2) / m2 * j_0_i(q, beta);
+        let t8 = -(a * a / m2 + m2 * 8.) * d_i_m_m_l_i_same_mass(q, om, p, m, beta);
+        let t9 = b * b / m2 * d_i_m_0_l_i(q, om, p, m, beta);
+        let t10 = -s * 2. * (s + m2 * 4.) * d_i_m_m_i_same_mass(q, om, p, m, beta);
+        let t11 = b * b * d_i_m_0_i(q, om, p, m, beta);
+        let t12 = (s + m2 * 3.) * d_j_m_i(q, m, beta);
+
+        let t13 = -m4 * d2_j_m_i(q, m, beta);
+        let t14 = (j_m_l_p_i - j_0_l_p_i) / m2;
+        let t15 = -d_j_m_l_p_i + d2_j_m_l_p_i * m2 / 2.;
+
+        (t14 + t15 + t13) + t12 + (t7 + (t6 + t1)) + t2 + t3 + t4 + t5 + t8 + t9 + t10 + t11
+    }
+
+    #[inline(always)]
+    pub fn thermal_polarization_t_landau_i<T: Num>(q: R, om: T, p: R, m: R, beta: R) -> C {
+        let s = om * om + p * p;
+        let s2 = s * s;
+        let m2 = m * m;
+        let m4 = m2 * m2;
+        let a = s + m2 * 2.;
+        let b = s + m2;
+
+        let t1 = (s2 * 3. / (m * 2.) - 1.) * i_0_0_t_i(q, om, p, beta);
+        let t2 = ((s2 * 3. + s * m2 * 8. + 4. * m4) / (2. * m2) + 4.)
+            * i_m_m_t_i_same_mass(q, om, p, m, beta);
+        let t3 = -(s2 * 3. + s * m2 * 4. + m4) / m4 * i_m_0_t_i(q, om, p, m, beta);
+        let t4 = a * s * 2. / m2 * i_m_m_i_same_mass(q, om, p, m, beta);
+        let t5 = -b * s * 2. / m2 * i_m_0_i(q, om, p, m, beta);
+        let t6 = -(s * 2. + m2 * 3.) / m2 * j_m_i(q, m, beta);
+        let t7 = (s * 2. + m2) / m2 * j_0_i(q, beta);
+        let t8 = -(a * a / m2 + m2 * 8.) * d_i_m_m_t_i_same_mass(q, om, p, m, beta);
+        let t9 = b * b / m2 * d_i_m_0_t_i(q, om, p, m, beta);
+        let t10 = -s * 2. * (s + m2 * 4.) * d_i_m_m_i_same_mass(q, om, p, m, beta);
+        let t11 = b * b * d_i_m_0_i(q, om, p, m, beta);
+        let t12 = (s + m2 * 3.) * d_j_m_i(q, m, beta);
+
+        let t13 = -m4 * d2_j_m_i(q, m, beta);
+        let t14 = (j_m_t_i(q, m, beta) - j_0_t_i(q, beta)) / m2;
+        let t15 = -d_j_m_t_i(q, m, beta) + d2_j_m_t_i(q, m, beta) * m2 / 2.;
+
+        t12 + (t7 + (t6 + t1)) + t2 + t3 + t4 + t5 + t8 + t9 + t10 + t11 + t14 + t15 + t13
+    }
+}
