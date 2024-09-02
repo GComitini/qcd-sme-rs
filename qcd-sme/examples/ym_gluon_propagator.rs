5b60b3d3febb27dfe2cf118360155a22eb8d60d6
Giorgio Comitini <giorgio.comitini@dfa.unict.it>
2024-09-02 17:56:12 +0200

    qcd-sme/examples/ym_gluon_propagator.rs: improve code readability

--- qcd-sme/examples/ym_gluon_propagator.rs
+++ qcd-sme/examples/ym_gluon_propagator.rs
@@ -10,15 +10,21 @@ use std::io::{BufWriter, Write};
 
 fn main() {
     /* DEFINITIONS: MOMENTA, TEMPERATURE, ETC. */
-    let (pmin, pmax) = (0.2, 3.);
+    let (pmin, pmax) = (0.2, 3.); // In GeV
     let prange = pmax - pmin;
-    let dp = 0.01;
+    let dp = 0.01; // In GeV
     let n = (prange / dp) as usize;
     let momenta: Vec<R> = (0..=n).map(|i| pmin + dp * (i as R)).collect();
-    let renpoint = 4.;
-    let om = 0.001;
+    let renpoint = 4.; // In GeV
+    let om = 0.001; // Matsubara frequency regulator, must be much less than pmin, in GeV
 
-    let temperatures = [0.121, 0.194, 0.260, 0.290, 0.366, 0.458];
+    let mg = 0.656;
+    let f0 = -0.876;
+
+    let mg2 = mg * mg;
+    let renpoint2 = renpoint * renpoint;
+
+    let temperatures = [0.121, 0.194, 0.260, 0.290, 0.366, 0.458]; // In GeV
     let ms_l = [0.525, 0.175, 0.025, 0.100, 0.350, 0.650]; // Suboptimal @ T = 260 MeV, m = 10 MeV is too low
     let f0s_l = [-0.05, -1.3, -4.4, -1.0, 1.2, 1.8]; // Suboptimal @ T = 260 MeV, see above
     let ms_t = [0.675, 0.725, 0.775, 0.725, 0.800, 0.900];
@@ -31,13 +37,12 @@ fn main() {
     ));
 
     /* VACUUM PROPAGATOR */
-    let norm =
-        propagator_landau((renpoint * renpoint) / (0.656 * 0.656), -0.876) * (renpoint * renpoint);
+    let norm = propagator_landau(renpoint2 / mg2, f0) * renpoint2;
     let vacuum_vals: Vec<R> = momenta
         .iter()
         .map(|&p| {
             eprint!("Computing (T, C, p) = (0, V, {p})... ");
-            let res = propagator_landau((p * p) / (0.656 * 0.656), -0.876) / norm;
+            let res = propagator_landau((p * p) / mg2, f0) / norm;
             eprintln!("Computed {res}.");
             res
         })
@@ -57,12 +62,12 @@ fn main() {
     plot.insert_image(vacuum_vals.clone());
     let mut legends = vec![String::from("$T=0$ MeV")];
 
-    for i in 0..=1 {
+    for i in 0..=2 {
         let t = temperatures[i];
         let beta = 1. / t;
         let m = ms_l[i];
-        let f0 = f0s_l[i] - 0.876;
-        let norm = propagator_l_landau(om, renpoint, m, beta, f0).re * (renpoint * renpoint);
+        let f0 = f0s_l[i] + f0;
+        let norm = propagator_l_landau(om, renpoint, m, beta, f0).re * renpoint2;
         let vals: Vec<R> = momenta
             .par_iter()
             .map(|&p| {
@@ -77,8 +82,8 @@ fn main() {
             fs::File::create(targetdir.join(&filename))
                 .expect(&format!("Could not create {filename}")),
         );
-        vals.iter().enumerate().for_each(|(i, d)| {
-            writeln!(file, "{}\t{d}", momenta[i]).ok();
+        momenta.iter().zip(vals.iter()).for_each(|(p, d)| {
+            writeln!(file, "{p}\t{d}").ok();
         });
         plot.insert_image(vals);
         legends.push(format!("$T={}$ MeV", (t * 1000.) as u32));
@@ -101,8 +106,8 @@ fn main() {
         let t = temperatures[i];
         let beta = 1. / t;
         let m = ms_l[i];
-        let f0 = f0s_l[i] - 0.876;
-        let norm = propagator_l_landau(om, renpoint, m, beta, f0).re * (renpoint * renpoint);
+        let f0 = f0s_l[i] + f0;
+        let norm = propagator_l_landau(om, renpoint, m, beta, f0).re * renpoint2;
         let vals: Vec<R> = momenta
             .par_iter()
             .map(|&p| {
@@ -117,8 +122,8 @@ fn main() {
             fs::File::create(targetdir.join(&filename))
                 .expect(&format!("Could not create {filename}")),
         );
-        vals.iter().enumerate().for_each(|(i, d)| {
-            writeln!(file, "{}\t{d}", momenta[i]).ok();
+        momenta.iter().zip(vals.iter()).for_each(|(p, d)| {
+            writeln!(file, "{p}\t{d}").ok();
         });
         plot.insert_image(vals);
         legends.push(format!("$T={}$ MeV", (t * 1000.) as u32));
@@ -144,8 +149,8 @@ fn main() {
         let t = temperatures[i];
         let beta = 1. / t;
         let m = ms_t[i];
-        let f0 = f0s_t[i] - 0.876;
-        let norm = propagator_t_landau(om, renpoint, m, beta, f0).re * (renpoint * renpoint);
+        let f0 = f0s_t[i] + f0;
+        let norm = propagator_t_landau(om, renpoint, m, beta, f0).re * renpoint2;
         let vals: Vec<R> = momenta
             .par_iter()
             .map(|&p| {
@@ -160,8 +165,8 @@ fn main() {
             fs::File::create(targetdir.join(&filename))
                 .expect(&format!("Could not create {filename}")),
         );
-        vals.iter().enumerate().for_each(|(i, d)| {
-            writeln!(file, "{}\t{d}", momenta[i]).ok();
+        momenta.iter().zip(vals.iter()).for_each(|(p, d)| {
+            writeln!(file, "{p}\t{d}").ok();
         });
         plot.insert_image(vals);
         legends.push(format!("$T={}$ MeV", (t * 1000.) as u32));
@@ -184,8 +189,8 @@ fn main() {
         let t = temperatures[i];
         let beta = 1. / t;
         let m = ms_t[i];
-        let f0 = f0s_t[i] - 0.876;
-        let norm = propagator_t_landau(om, renpoint, m, beta, f0).re * (renpoint * renpoint);
+        let f0 = f0s_t[i] + f0;
+        let norm = propagator_t_landau(om, renpoint, m, beta, f0).re * renpoint2;
         let vals: Vec<R> = momenta
             .par_iter()
             .map(|&p| {
@@ -200,8 +205,8 @@ fn main() {
             fs::File::create(targetdir.join(&filename))
                 .expect(&format!("Could not create {filename}")),
         );
-        vals.iter().enumerate().for_each(|(i, d)| {
-            writeln!(file, "{}\t{d}", momenta[i]).ok();
+        momenta.iter().zip(vals.iter()).for_each(|(p, d)| {
+            writeln!(file, "{p}\t{d}").ok();
         });
         plot.insert_image(vals);
         legends.push(format!("$T={}$ MeV", (t * 1000.) as u32));
