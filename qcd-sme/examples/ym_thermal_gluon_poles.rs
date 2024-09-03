36fc56f71576ee6bb72fcf3dbbad62c18934fd60
Giorgio Comitini <giorgio.comitini@dfa.unict.it>
2024-09-03 10:15:07 +0200

    qcd-sme/examples/ym_thermal_gluon_poles.rs: clean up code

--- qcd-sme/examples/ym_thermal_gluon_poles.rs
+++ qcd-sme/examples/ym_thermal_gluon_poles.rs
@@ -3,6 +3,8 @@ use qcd_sme::ym::gluon::dressing_inv_landau;
 use qcd_sme::ym::thermal::zero_momentum::gluon::dressing_t_inv_landau;
 use qcd_sme::{C, I, R};
 
+const MG: R = 0.656;
+
 const TOL: R = 1E-12;
 const MAX_ITER: usize = 1000000;
 const Z0: C = C::new(-1., -1.);
@@ -12,6 +14,7 @@ fn find_root<F: Fn(C) -> C>(f: &F) -> Option<(C, C)> {
     qcd_sme::utils::find_root(f, (Z0, Z1), TOL, MAX_ITER)
 }
 
+// This actually finds zeroes
 fn find_pole<F: Fn(C) -> C>(header: &str, f: &F, res: &mut Vec<(C, R)>) {
     println!("{header}\n");
     let root = find_root(f);
@@ -27,16 +30,17 @@ fn find_pole<F: Fn(C) -> C>(header: &str, f: &F, res: &mut Vec<(C, R)>) {
     let residue = qcd_sme::utils::compute_residue(&|z| 1. / f(z), root) / root;
     let (z_r, z_i) = (residue.re, residue.im);
     let z_rat = z_i / z_r;
-    let root = (-root).sqrt() * 0.656;
+    let root = (-root).sqrt() * MG;
     res.push((root, z_rat));
     println!(
-        "For m = 0.656 GeV, this is equal to p = ({} + {} i) GeV.",
+        "For m = {MG:.3} GeV, this is equal to p = ({} + {} i) GeV.",
         root.re, root.im
     );
     println!("The residue of the inverse is equal to {residue}, with im/re ratio = {z_rat}");
     println!();
 }
 
+// This actually finds zeroes
 fn find_thermal_pole<F: Fn(C) -> C>(header: &str, f: &F, res: &mut Vec<(C, R)>) {
     println!("{header}\n");
     let root = find_root(f);
@@ -49,17 +53,18 @@ fn find_thermal_pole<F: Fn(C) -> C>(header: &str, f: &F, res: &mut Vec<(C, R)>) 
             panic!("Found no root");
         }
     };
-    // 2*root is the conversion factor from a pole in p^2 to a pole in p
+    // +/- 2*root is the conversion factor from a pole in p to a pole in p^2
     // 1/root^2 gives us the residue of the propagator instead of that of the dressing function
-    // -residue.im selects the pole on the same quadrant as that found by find_pole
+    // -residue.im and conj() select the pole on the first quadrant
     let residue = qcd_sme::utils::compute_residue(&|z| 1. / f(z), root) * 2. / root;
-    let (z_r, z_i) = (residue.re, -residue.im);
+    let residue = residue.conj();
+    let (z_r, z_i) = (residue.re, residue.im);
     let z_rat = z_i / z_r;
-    let root = root * I;
+    let root = (root * I).conj();
     res.push((root, z_rat));
     println!(
         "This is equal to epsilon = {} GeV, gamma = {} GeV.",
-        root.re, -root.im
+        root.re, root.im
     );
     println!("The residue of the inverse is equal to {residue}, with im/re ratio = {z_rat}");
     println!();
@@ -68,24 +73,26 @@ fn find_thermal_pole<F: Fn(C) -> C>(header: &str, f: &F, res: &mut Vec<(C, R)>) 
 fn main() {
     let mut res = Vec::new();
 
+    let f0 = -0.876;
+
     let temperatures = [0., 0.121, 0.194, 0.260, 0.290, 0.366, 0.458];
     let masses = [0.675, 0.725, 0.775, 0.725, 0.800, 0.900];
-    let f0s = [0.05, 0.10, 0.30, 0.40, 0.50, 0.60];
+    let df0s = [0.05, 0.10, 0.30, 0.40, 0.50, 0.60];
 
     find_pole(
         "*** VACUUM POLE ***",
-        &|z: C| dressing_inv_landau(z, -0.876),
+        &|z: C| dressing_inv_landau(z, f0),
         &mut res,
     );
 
-    for (&t, (&m, &f0)) in temperatures
+    for (&t, (&m, &df0)) in temperatures
         .iter()
         .skip(1)
-        .zip(masses.iter().zip(f0s.iter()))
+        .zip(masses.iter().zip(df0s.iter()))
     {
         find_thermal_pole(
             &format!("*** T = {} MeV POLE ***", (t * 1000.) as u32),
-            &|z: C| dressing_t_inv_landau(z, m, 1. / t, f0 - 0.876),
+            &|z: C| dressing_t_inv_landau(z, m, 1. / t, df0 + f0),
             &mut res,
         );
     }
